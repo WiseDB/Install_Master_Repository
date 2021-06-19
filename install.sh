b56d54268d0009d353df9890fb6ff7be4ba0fa6e
Rodrigo Almeida <rodrigo.almeida@wisedb.com.br>
2021-06-19 18:40:26 +0000

    script v1.0

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+clear;
+echo -e "Instalação do repositório da Wise Database Solutions"
+echo -e "••••••••••••••••••••••••••••••••••••••••••••••••••••\n"
 echo -e -n "Nome do diretório BASE da Wise (ex: /opt/WiseDb): "
 read WDIR
 echo -e -n "Nome do usuário ADMIN da Wise (ex: admin_wise): "
@@ -13,10 +16,30 @@ groupadd wisedb
 useradd $WISE_ADMIN_USER -g wisedb -G wheel
 usermod --password $(openssl passwd -1 nomanager) $WISE_ADMIN_USER
 
-su - $WISE_ADMIN_USER
-ssh-keygen -b 2048 -f ~/.ssh/id_rsa -t rsa -q -N ""
-echo -e "\n\nEntre no repositório da Wise: https://github.com/WiseDB/Customer_Master/settings/keys"
-echo -e "e adicione a chave abaixo:\n"
-cat ~/.ssh/id_rsa.pub
-read -n 1 -s -r -p "\nTecle algo para continuar a instalação ou CTRL+C para cancelar."
-exit
+# Criação dos usuários da WiseDb
+#useradd rodrigo_wise -g wisedb -G wheel,oinstall,dba
+#usermod --password $(openssl passwd -1 nomanager) rodrigo_wise
+#useradd fernando_wise -g wisedb -G wheel,oinstall,dba
+#usermod --password $(openssl passwd -1 nomanager) fernando_wise
+#useradd caio_wise -g wisedb -G oinstall,dba
+#usermod --password $(openssl passwd -1 nomanager) caio_wise
+
+sudo -H -u $WISE_ADMIN_USER bash -c 'ssh-keygen -b 2048 -f ~/.ssh/id_rsa -t rsa -q -N ""'
+sudo -H -u $WISE_ADMIN_USER bash -c 'echo -e "\n\nEntre no repositório da Wise: \e[91mhttps://github.com/WiseDB/Customer_Master/settings/keys\e[0m"'
+sudo -H -u $WISE_ADMIN_USER bash -c 'echo -e "E adicione a chave abaixo:\n"'
+sudo -H -u $WISE_ADMIN_USER bash -c 'cat ~/.ssh/id_rsa.pub'
+echo ""
+read -n 1 -s -r -p "Após adicionar a chave SSH, tecle algo para continuar..."
+
+# Criação e permissão do diretório
+mkdir -p $WISE_BASE_DIR
+chown -R $WISE_ADMIN_USER.wisedb $WISE_BASE_DIR
+chmod g+w $WISE_BASE_DIR
+
+sudo -H -u $WISE_ADMIN_USER bash -c 'git clone git@github.com:WiseDb/Customer_Master.git $WISE_BASE $WISE_BASE_DIR'
+
+# Configurações finais do git
+sudo -H -u $WISE_ADMIN_USER bash -c 'git config --global push.default simple'
+sudo -H -u $WISE_ADMIN_USER bash -c 'git config --global user.email "wisedbadm@gmail.com"'
+sudo -H -u $WISE_ADMIN_USER bash -c 'git config --global user.name  "Admin WiseDb"'
+
