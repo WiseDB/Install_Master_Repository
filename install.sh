391e7e0a14547416d6eb2d702a7841d579fb8b41
Rodrigo Almeida <rodrigo.almeida@wisedb.com.br>
2021-06-20 14:26:42 +0000

    pequenos ajustes

--- install.sh
+++ install.sh
@@ -24,9 +24,9 @@ export WISE_ADMIN_GROUP=$WGROUP
 
 
 echo -e "\nInstalando pacotes necessários\n"
-yum -y install screen
-yum -y install git
-yum -y install mutt
+#yum -y install screen
+#yum -y install git
+#yum -y install mutt
 
 # Create Groups and Users
 echo -e "\nCriando estrutura de Usuários, Grupos e Diretórios\n"
@@ -39,26 +39,22 @@ useradd $WISE_ADMIN_USER -g $WISE_ADMIN_GROUP -G wheel,oinstall && usermod --pas
 #useradd caio_wise -g $WISE_ADMIN_GROUP -G oinstall
 #usermod --password $(openssl passwd -1 nomanager) caio_wise
 
+# Criação e permissão do diretório
+mkdir -p $WISE_BASE_DIR
+chown -R $WISE_ADMIN_USER.wisedb $WISE_BASE_DIR
+chmod g+w $WISE_BASE_DIR
+
 sudo -H -u $WISE_ADMIN_USER bash -c 'ssh-keygen -b 2048 -f ~/.ssh/id_rsa -t rsa -q -N ""'
 sudo -H -u $WISE_ADMIN_USER bash -c 'echo -e "\n\nEntre no repositório da Wise: \e[91mhttps://github.com/WiseDB/Customer_Master/settings/keys\e[0m"'
 sudo -H -u $WISE_ADMIN_USER bash -c 'echo -e "E adicione a chave abaixo:\n"'
 sudo -H -u $WISE_ADMIN_USER bash -c 'cat ~/.ssh/id_rsa.pub'
 echo ""
 read -n 1 -s -r -p "Após adicionar a chave SSH, tecle algo para continuar..."
-
-# Criação e permissão do diretório
-mkdir -p $WISE_BASE_DIR
-chown -R $WISE_ADMIN_USER.wisedb $WISE_BASE_DIR
-chmod g+w $WISE_BASE_DIR
-
 echo ""
-ls -lhd $WISE_BASE_DIR
-echo "ADMIN_USER: $WISE_ADMIN_USER"
-read -n 1 -s -r -p "Press any key to continue..."
 
 echo -e "\e[91m"
 echo -e "\n\nIMPORTANTE: Digite o comando abaixo para clonar o repositório com o usuário $WISE_ADMIN_USER.\n"
-echo -e "git clone git@github.com:WiseDb/Customer_Master.git $WISE_BASE_DIR ./bin/install.sh"
+echo -e "git clone git@github.com:WiseDb/Customer_Master.git $WISE_BASE_DIR && $WISE_BASE_DIR/bin/install.sh"
 echo -e "\e[0m"
 su - $WISE_ADMIN_USER
 
