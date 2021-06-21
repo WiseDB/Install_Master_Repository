8315ac050031b20c262300ff60d35af13c9dfa1f
Rodrigo Almeida <rodrigo.pe@gmail.com>
2021-06-20 23:52:57 -0300

    configuração do oracle (mensagens)

--- install.sh
+++ install.sh
@@ -79,15 +79,39 @@ read -n 1 -s -r -p "Após adicionar a chave SSH, tecle algo para continuar..."
 echo ""
 
 #GIT_COMMAND="git clone git@github.com:WiseDb/Customer_Master.git $WISE_BASE_DIR && $WISE_BASE_DIR/bin/install.sh $WISE_BASE_DIR"  
-GIT_COMMAND="cd $WISE_BASE_DIR && git pull origin master"  
-INSTALL_SCRIPT=/home/$WISE_ADMIN_USER/install_wise.sh
+GIT_COMMAND="cd $WISE_BASE_DIR && git pull origin master && exit"  
+INSTALL_SCRIPT=/home/$WISE_ADMIN_USER/download_master.sh
 echo -e "$GIT_COMMAND" > $INSTALL_SCRIPT
 chown $WISE_ADMIN_USER.$WISE_ADMIN_GROUP $INSTALL_SCRIPT
 chmod 755 $INSTALL_SCRIPT
-echo -e "\n\nExecute o script \"install.sh\" para finalizar a instalação.\n"
+echo -e "\n\nExecute o script \"download_master.sh\" para finalizar a instalação.\n"
 #echo -e "\e[91m"
 echo -e "digite:\e[91m ~/install_wise.sh \e[0m\n" 
 #echo -e "\e[0m"
 su $WISE_ADMIN_USER
+
+su oracle
+clear
+echo -e "Você está logado com o usuário \"oracle\""
+echo -e "Siga os passos abaixo para criar a área de configuração do cliente:\n"
+echo -e "  1) Acesse o link do template\e[34m https://github.com/WiseDB/Customer_Template\e[0m.\n"
+echo -e "  2) Clique no botão\e[34m [Use this template]\e[0m para criar o repositório do cliente.\n"
+echo -e "  3) Escolha um nome para o repositório (privado), com o formato:\e[34m Customer_NomeEmpresa\e[0m\n"
+echo -e "  4) Adicione a chave SSH do usuário \"oracle\" ao novo repositório."
+echo -e "     Atenção: Use a opção de permitir gravação"
+echo -e "     Digite o caminho para página das chaves SSH:"
+echo -e "     \e[34mhttps://github.com/WiseDB/Customer_NomeEmpresa/settings/keys\e[0m"
+echo -e "     $(cat /home/oracle/.ssh/id_rsa.pub)\n"
+echo -e "  5) Crie um clone através do comando abaixo:"
+echo -e "     git clone git@github.com:WiseDB/Customer_NomeEmpresa.git $WISE_BASE_DIR/customer\n"
+echo -e "  6) Entre no diretório de configuração: "
+echo -e "     cd $WISE_BASE_DIR/customer/config\n"
+echo -e "  7) Copie o template \e[34m.customer_info.cfg.template\e[0m para um arquivo de nome \e[34mcustomer.cfg\e[0m"
+echo -e "     e faça as configurações necessárias.\n"
+echo -e "  8) Copie o template \e[34m.SID.db.cfg.template\e[0m para um arquivo no formato \e[34mSID.db.cfg\e[0m"
+echo -e "     e faça as configurações necessárias."
+echo -e "     OBS: Troque a string \"SID\" pelo nome apropriado da instância.\n"
+
 echo -e "Voltando ao root."
+
 
