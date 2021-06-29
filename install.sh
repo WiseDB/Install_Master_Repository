ca4ff36bd4015d5efd07debc1752978394dc2b8f
Rodrigo Almeida <rodrigo.pe@gmail.com>
2021-06-29 16:11:35 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -114,8 +114,10 @@ if [ $(crontab -l -u $WISE_ADMIN_USER |wc -l) -eq 0 ]; then
 	cat $WISE_BASE_DIR/bin/crontab_header.txt | crontab -u $WISE_ADMIN_USER -
 fi
 # Comando para sincronismo do repositorio master
-sudo -i -u $WISE_ADMIN_USER $WISE_BASE_DIR/bin/add_to_crontab.sh "*/30 *    *    *    *   " "export WISE_BASE_DIR=$WISE_BASE_DIR && $WISE_BASE_DIR/bin/auto_update_master.sh"
-sudo -i -u oracle           $WISE_BASE_DIR/bin/add_to_crontab.sh "*/31 *    *    *    *   " "export WISE_BASE_DIR=$WISE_BASE_DIR && $WISE_BASE_DIR/bin/auto_update_customer.sh"
+echo -e "WISE_ADMIN_USER: $WISE_ADMIN_USER"
+echo -e "WISE_BASE_DIR..: $WISE_BASE_DIR"
+sudo -i -u $WISE_ADMIN_USER $WISE_BASE_DIR/bin/add_to_crontab.sh "*/31 *    *    *    *   " "export WISE_BASE_DIR=$WISE_BASE_DIR && $WISE_BASE_DIR/bin/auto_update_master.sh"
+sudo -i -u oracle           $WISE_BASE_DIR/bin/add_to_crontab.sh "*/32 *    *    *    *   " "export WISE_BASE_DIR=$WISE_BASE_DIR && $WISE_BASE_DIR/bin/auto_update_customer.sh"
 
 ####################################################################
 # Criação da área do cliente
